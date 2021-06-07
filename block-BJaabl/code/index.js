f2f6f98b169e230a029880f0bacc21cdbdfcca69
akk26 <anujbph@gmail.com>
2021-06-07 23:41:50 +0530

    Completed assignment

--- block-BJaabl/code/index.js
+++ block-BJaabl/code/index.js
@@ -2,6 +2,11 @@
 1. Create a function named `sayHello` that that accepts a parameter `name` and alert `Hello [name]!`.
 */
 
+function sayHello(name){
+  alert(`Hello ${name}`)
+}
+
+
 /*
 2. Create a function named `getFullName` that accepts two parameter `firstName` and `lastName` and returns
 fullName.
@@ -11,15 +16,30 @@ Example:
   getFullName("Nelson", "Mandela"); // "Nelson Mandela"
 */
 
+function getFullName(firstName, LastName){
+  let fullName = (`${firstName}${LastName}`)
+  return(fullName)
+}
+
+
 /*
 3. Create a function named `addTwoNumbers` that accepts two numbers i.e `firstNum` and `secondNum` and returns
 the sum of both numbers. If user passes any data other then numbers alert a message saying `Enter Valid Input`
 
 Example:
-addTwoNumbers(10, 22); // 32
-addTwoNumbers(20, 32); // 32
-addTwoNumbers(10, "100"); // Alert Enter Valid Input
-*/
+// addTwoNumbers(10, 22); // 32
+// addTwoNumbers(20, 32); // 32
+// addTwoNumbers(10, "100"); // Alert Enter Valid Input
+// */
+
+function addTwoNumbers(firstNum, secondNum){
+  if(typeof firstNum !== "number" || typeof secondNum !== "number"){
+    alert("Enter a Valid Input")
+  }
+  else{
+    return(firstNum + secondNum);
+  }
+}
 
 /*
 4. Create a function named `calc` which accepts three parameter `numA`, `numB` and `operation`. Operation
@@ -42,7 +62,22 @@ isLeapYear(2000); // true
 isLeapYear(2001); // false
 */
 
+function isLeapYear() {
+ let year = +prompt("Enter Year?");
+ if(year % 4 === 0 ){
+   return("true")
+ } else{
+   return("false")
+ }
+}
 
 /*
 6. Create a function named `getFactorial` that accepts a number and return the factorial of the number.
 */
+function getFactorial(number){
+  let factorial = 1;
+  for(let i = number; i >= 1; i-- ){
+    factorial = factorial * i
+  }
+  return(factorial);
+}
